e7503ae320a6bedd85727f1fbc67abc9dee18299
Iftekhar Md. Shishir <84434133+iftekharweb@users.noreply.github.com>
2023-02-05 16:09:48 +0600

    Update Lazy_Propagation.cpp

--- Lazy_Propagation.cpp
+++ Lazy_Propagation.cpp
@@ -64,3 +64,65 @@ void BuildTree(ll si , ll ss , ll se) {
         return;
 }
 
+2. 
+        
+void RangeUpdate(int si , int ss , int se , int li , int ri , int val,vector<int>& Tree,vector<int>& lazy) {
+        // Is there any pending update? 
+        if(lazy[si]) {
+            int dx = lazy[si];
+            lazy[si] = 0;
+            Tree[si] += dx*(se-ss+1);
+            if(ss != se) {
+                lazy[2*si] += dx;
+                lazy[2*si+1] += dx;
+            }
+        }
+        // Do the rest..
+        if(li>se || ri<ss) return;
+        if(li<=ss && ri>=se) {
+            int dx = val * (se-ss+1);
+            Tree[si] += dx;
+            if(ss != se) {
+                lazy[2*si] += val;
+                lazy[2*si+1] += val;
+            }
+            return;
+        }
+        int mid = (ss+se)>>1;
+        RangeUpdate(2*si, ss, mid , li , ri , val,Tree,lazy);
+        RangeUpdate(2*si+1, mid+1 , se , li , ri , val,Tree,lazy);
+        Tree[si] = Tree[2*si]+Tree[2*si+1];
+        return;
+}
+ 
+int Query(int si , int ss, int se , int li , int ri, vector<int>& Tree,vector<int>& lazy) {
+        // Is there any pending update? 
+        if(lazy[si]) {
+            int dx = lazy[si];
+            lazy[si] = 0;
+            Tree[si] += dx*(se-ss+1);
+            if(ss != se) {
+                lazy[2*si] += dx;
+                lazy[2*si+1] += dx;
+            }
+        }
+        // Do the rest..
+        if(li>se || ri<ss) return 0;
+        if(li<=ss && ri>=se) {
+            return Tree[si];
+        }
+        int mid = (ss+se)>>1;
+        return Query(2*si, ss , mid , li , ri,Tree,lazy) + Query(2*si+1, mid+1, se, li, ri,Tree,lazy);
+}
+ 
+void BuildTree(int si , int ss , int se, vector<int>& Tree,vector<int>& lazy,vector<int>& a) {
+        if(ss == se) {
+            Tree[si] = a[ss];
+            return;
+        }
+        int mid = (ss+se)>>1;
+        BuildTree(2*si, ss , mid, Tree,lazy,a);
+        BuildTree(2*si+1, mid+1 , se, Tree,lazy,a);
+        Tree[si] = Tree[2*si]+Tree[2*si+1];
+        return;
+}
